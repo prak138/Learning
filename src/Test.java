863f88e31856cd7dd8ba1ad2dde417948a50a7cd
Z002U2WR <Z002U2WR@AAEINGGN01737L.ad001.siemens.net>
2018-01-22 18:30:09 +0530

    INIT

---
+++ src/Test.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) Siemens AG 2018 ALL RIGHTS RESERVED.
+ *
+ * SPPA-T3000  
+ * 
+ */
+
+public class Test
+{
+    
+}
+
+
+/*
+ * Copyright (c) Siemens AG 2018 ALL RIGHTS RESERVED
+ *
+ * SPPA-T3000
+ */
